ee9d62484e0174fca5d338b2cfdf3f68d2687f89
Tony Bowden <tony@tmtm.com>
2017-04-30 12:12:43 +0100

    DROP TABLE on each run

--- scraper.rb
+++ scraper.rb
@@ -107,5 +107,5 @@ def scrape_mp(url)
   ScraperWiki.save_sqlite(%i[id term], data)
 end
 
-ScraperWiki.sqliteexecute('DELETE FROM data') rescue nil
+ScraperWiki.sqliteexecute('DROP TABLE data') rescue nil
 scrape_list 'http://www.parliament.gh/parliamentarians'
