b5680a9a3692489bae4f05f9d6b9b5b9c235dfdf
Oliver Denman <oliver@mysociety.org>
2017-06-28 14:25:54 +0100

    Return list of members page urls
    Instead of returning the 'next' members list page, the members page class now
    returns a list of all members pages Urls. This is representative of what the
    page actually displays. Members pages no longer provide previous/next links.

--- lib/members_page.rb
+++ lib/members_page.rb
@@ -4,13 +4,15 @@ require 'scraped'
 require_relative './member_card'
 
 class MembersPage < Scraped::HTML
+  decorator Scraped::Response::Decorator::CleanUrls
+
   field :member_cards do
     noko.css('.card').map do |card|
       (fragment card => MemberCard).to_h
     end
   end
 
-  field :next_page do
-    noko.css('span.content_txt').xpath('.//a[contains(text(),">")]/@href').text
+  field :members_pages_urls do
+    noko.css('.square @href').map(&:text)
   end
 end
