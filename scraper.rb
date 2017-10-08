5353a6704b8a6cfbd37791eea0d37009403cf6c1
Tony Bowden <tony@tmtm.com>
2017-10-08 10:28:55 +0100

    Don't include term ID
    This isn't on the page

--- scraper.rb
+++ scraper.rb
@@ -20,7 +20,7 @@ end
 def member_data(url)
   page = scrape url => MembersPage
   page.member_cards.map do |mp_card|
-    mp_card.merge((scrape mp_card[:source] => MemberPage).to_h).merge(term: 6)
+    mp_card.merge((scrape mp_card[:source] => MemberPage).to_h)
   end
 end
 
@@ -28,7 +28,7 @@ starting_url = 'https://www.parliament.gh/mps?az&filter=all'
 data = (scrape starting_url => MembersPage).members_pages_urls.flat_map do |url|
   member_data(url)
 end
-
 data.each { |mem| puts mem.reject { |_, v| v.to_s.empty? }.sort_by { |k, _| k }.to_h } if ENV['MORPH_DEBUG']
+
 ScraperWiki.sqliteexecute('DROP TABLE data') rescue nil
-ScraperWiki.save_sqlite(%i[id term], data)
+ScraperWiki.save_sqlite(%i[id], data)
